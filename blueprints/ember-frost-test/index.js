16ff15572379b85618b2ce470787bda7e5142632
Michael Carroll <juwara0@users.noreply.github.com>
2017-12-21 09:01:23 -0600

    added addPackagesToProject method that respects save-exact flag

--- blueprints/ember-frost-test/index.js
+++ blueprints/ember-frost-test/index.js
@@ -1,7 +1,34 @@
 /**
  * Install blueprint for ember-frost-test addon
  */
+
+const Promise = require('bluebird')
+const chalk = require('chalk')
+const cpExec = require('child_process').exec
+const exec = Promise.promisify(cpExec)
+
 module.exports = {
+
+  /*
+   * Our own addPackagesToProject method that respects the save-exact flag
+   * @function
+   * @override
+   * @param {Object[]} packagesToAdd - This is the package name, target and optional save-exact flag
+   * @returns {Promise} - npm commands to be executed
+   */
+  addPackagesToProject: function (packagesToAdd) {
+    const installCommands = []
+
+    packagesToAdd.forEach((pkg) => {
+      this.ui.writeLine(chalk.green('Installing package') + ` ${pkg.name}`)
+      installCommands.push(
+        exec(`npm install --save-dev ${pkg.name}@${pkg.target} ${pkg.saveExact ? '--save-exact' : ''}`)
+      )
+    })
+
+    return Promise.all(installCommands)
+  },
+
   afterInstall: function () {
     const addonsToAdd = {
       packages: [
@@ -15,8 +42,8 @@ module.exports = {
     }
 
     const packagesToAdd = [
-      {name: 'ember-cli-mocha', target: '0.14.4', 'save-exact': true},
-      {name: 'ember-cli-chai', target: '0.4.3', 'save-exact': true},
+      {name: 'ember-cli-mocha', target: '0.14.4', 'saveExact': true},
+      {name: 'ember-cli-chai', target: '0.4.3', 'saveExact': true},
       {name: 'sinon-chai', target: '^2.14.0'},
       {name: 'chai-jquery', target: '^2.0.0'}
     ]
