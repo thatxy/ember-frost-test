88c14f89425f039937f60790deffcde3e30feef5
Michael Carroll <juwara0@users.noreply.github.com>
2017-12-21 09:14:26 -0600

    migrate rest of packages to addPackagesToProject method since no blueprints are needing to be ran

--- blueprints/ember-frost-test/index.js
+++ blueprints/ember-frost-test/index.js
@@ -30,28 +30,17 @@ module.exports = {
   },
 
   afterInstall: function () {
-    const addonsToAdd = {
-      packages: [
-        {name: 'ember-hook', target: '1.4.2'},
-        {name: 'ember-sinon', target: '^0.7.0'},
-        {name: 'ember-test-utils', target: '^8.0.0'}
-      ],
-      blueprintOptions: {
-        saveExact: true
-      }
-    }
-
     const packagesToAdd = [
       {name: 'ember-cli-mocha', target: '0.14.4', 'saveExact': true},
       {name: 'ember-cli-chai', target: '0.4.3', 'saveExact': true},
+      {name: 'ember-hook', target: '1.4.2'},
+      {name: 'ember-sinon', target: '^0.7.0'},
+      {name: 'ember-test-utils', target: '^8.0.0'},
       {name: 'sinon-chai', target: '^2.14.0'},
       {name: 'chai-jquery', target: '^2.0.0'}
     ]
 
-    return this.addAddonsToProject(addonsToAdd)
-      .then(() => {
-        return this.addPackagesToProject(packagesToAdd)
-      })
+    return this.addPackagesToProject(packagesToAdd)
   },
 
   normalizeEntityName: function () {
